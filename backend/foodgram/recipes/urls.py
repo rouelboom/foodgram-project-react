671fa846df326304429d7878efa01e4dc07a1410
Павел Сенокосов <pavel@ya.ru>
2021-11-07 22:55:54 +0500

    update serializers, add validation

--- backend/foodgram/recipes/urls.py
+++ backend/foodgram/recipes/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'tags', views.TagViewSet, basename='tags')
-router.register(r'ingredients', views.IngredientViewSet, basename='ingredients')
+router.register(r'tags', views.TagList, basename='tags')
+router.register(r'ingredients', views.IngredientList, basename='ingredients')
 router.register(r'recipes', views.RecipeViewSet, basename='recipes')
 
 urlpatterns = router.urls
